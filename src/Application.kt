f1d6f806b9ec07f91360f3f1511c386c126f984c
Shengyou Fan <shengyoufan@gmail.com>
2019-08-16 11:10:36 +0800

    serve static content

--- src/Application.kt
+++ src/Application.kt
@@ -3,6 +3,8 @@ package com.example
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.html.respondHtml
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import kotlinx.html.*
@@ -14,6 +16,10 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module(testing: Boolean = false) {
 
     routing {
+        static("static") {
+            resources("static")
+        }
+
         get("/") {
             call.respondHtml {
                 head {
