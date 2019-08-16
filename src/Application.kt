bca3deee72447ed8381857d49843478d543a5ad1
Shengyou Fan <shengyoufan@gmail.com>
2019-08-16 11:28:32 +0800

    serve page using FreeMarker

--- src/Application.kt
+++ src/Application.kt
@@ -1,10 +1,15 @@
 package com.example
 
+import freemarker.cache.ClassTemplateLoader
 import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.freemarker.FreeMarker
+import io.ktor.freemarker.FreeMarkerContent
 import io.ktor.html.respondHtml
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
+import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import kotlinx.html.*
@@ -15,25 +20,22 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
 
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+    }
+
     routing {
         static("static") {
             resources("static")
         }
 
         get("/") {
-            call.respondHtml {
-                head {
-                    title { +"Hello, HTML DSL" }
-                }
-                body {
-                    h1 { +"HTML DSL" }
-                    ul {
-                        li { +"Item 1" }
-                        li { +"Item 2" }
-                        li { +"Item 3" }
-                    }
-                }
-            }
+            call.respond(
+                FreeMarkerContent(
+                    "index.ftl",
+                    mapOf("title" to "Hello, world")
+                )
+            )
         }
     }
 
