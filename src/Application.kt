ca41b30744161c273baceb3ab38f98dac661ed33
Shengyou Fan <shengyoufan@gmail.com>
2019-08-16 13:32:48 +0800

    error handling

--- src/Application.kt
+++ src/Application.kt
@@ -6,6 +6,8 @@ import freemarker.cache.ClassTemplateLoader
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.features.StatusPages
+import io.ktor.features.statusFile
 import io.ktor.freemarker.FreeMarker
 import io.ktor.freemarker.FreeMarkerContent
 import io.ktor.http.HttpStatusCode
@@ -26,6 +28,18 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    install(StatusPages) {
+        exception<Throwable> { cause ->
+            call.respond(HttpStatusCode.InternalServerError)
+        }
+
+        statusFile(
+            HttpStatusCode.NotFound,
+            HttpStatusCode.InternalServerError,
+            filePattern = "/errors/error-#.html"
+        )
+    }
 
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
