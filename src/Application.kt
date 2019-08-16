6de71d96ca433b361624c5a02e207aaf5bdaf6df
Shengyou Fan <shengyoufan@gmail.com>
2019-08-16 10:58:53 +0800

    response "Hello, world" text

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,28 @@
 package com.example
 
 import io.ktor.application.*
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    routing {
+        get("/") {
+            call.respondText(
+                "Hello, world",
+                ContentType.Text.Plain,
+                HttpStatusCode.OK
+            )
+        }
+    }
+
 }
 
